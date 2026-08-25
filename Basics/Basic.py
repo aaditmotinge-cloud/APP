7ca32316517f57cc1ec4c60e363800e13d972e17
Aadit Motinge <aaditmotinge@gmail.com>
2026-08-25 11:23:10 +0530

    Implement basic operations in Basic.py
    Added a basic Python program that performs various operations including addition, square and cube calculation, leap year check, and grade evaluation.

---
+++ Basics/Basic.py
@@ -0,0 +1,44 @@
+print("Hello, World!")
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("The sum of the code is",a+b)
+
+num=int(input("Enter the number"))
+sq=(num*num)
+cube= (num*num*num)
+print("The square of the number is",sq,"and the cube is", cube)
+
+year=int(input("Enter the year to be checked:"))
+if year%4==0:
+    print("It is leap year")
+else:
+    print("It is not the leap year")
+    
+marks=int(input("Enter your marks:"))
+if marks>=91:
+    print("your grade is O")
+elif marks>=81:
+    print("Your grade is A+")
+elif marks>=71:
+    print("Your grade is A")
+elif marks>=61:
+    print("Your grade is B+")
+elif marks>=51:
+    print("Your grade is B")
+elif marks>=41:
+    print("Your grade is C")   
+elif marks>=31:
+    print("Your grade is D")
+elif marks>=21:
+    print("Your grade is E")
+else:
+    print("You are FAIL")
+          
+vwls=[a,e,i,o,u,]
+word=input("Enter the word:")
+if f[0] in word == vwls[0][1][2][3][4]:
+    print("It is a vowel")
+else:
+    print("It is a consonants")
+    
